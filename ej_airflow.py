805123547b64ed562801e36acda785a01126db08
David Carlos <ddavidcarlos1392@gmail.com>
2020-04-16 21:58:48 -0300

    WIP: Add operator to merge EJ and Mautic data

--- ej_airflow.py
+++ ej_airflow.py
@@ -1,18 +1,40 @@
 from airflow.models import DAG
 from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.operators.python_operator import PythonOperator
+import json
 import datetime
 
 dag = DAG('osf_pipeline')
+
+
+def merge_data(**context):
+    ej_votes_list = json.loads(context['task_instance'].xcom_pull(
+        task_ids='request_ej_reports_data'))
+    mautic_data = json.loads(context['task_instance'].xcom_pull(
+        task_ids='request_mautic_data'))
+    mautic_contact = mautic_data["contacts"]["1"]
+    merge_data = {}
+    for vote in ej_votes_list:
+        if(len(vote["email"].split('-')) > 1):
+            mtc_id = vote["email"].split('-')[0]
+            label = vote["email"].split('-')[1]
+            if(mtc_id == "1" and label == 'mautic@mail.com'):
+                merge_data = {**vote, **mautic_contact}
+                break
+        continue
+    return merge_data
+
 
 t1 = SimpleHttpOperator(
     start_date=datetime.datetime.now(),
     task_id="request_ej_reports_data",
     http_conn_id="ej_api",
-    endpoint='/api/v1/conversations/1/reports?fmt=csv&&export=votes',
+    endpoint='/api/v1/conversations/1/reports?fmt=json&&export=votes',
     method="GET",
     headers={"Accept": "text/csv"},
     response_check=lambda response: True if response.content else False,
     log_response=True,
+    xcom_push=True,
     dag=dag)
 
 
@@ -22,8 +44,20 @@ t2 = SimpleHttpOperator(
     http_conn_id="mautic_api",
     endpoint='/api/contacts/',
     method="GET",
-    headers={"Authorization": "Basic cGVuY2lsbGFiczpAbTR1NzE1"},
+    headers={"Authorization": "Basic ZGF2aWRjYXJsb3M6RGF2aWRAMTNjYQ=="},
     response_check=lambda response: True if response.content else False,
     log_response=True,
+    xcom_push=True,
     dag=dag)
-[t1, t2]
+
+t3 = PythonOperator(
+    start_date=datetime.datetime.now(),
+    provide_context=True,
+    python_callable=merge_data,
+    task_id="merge_ej_mautic_data",
+    xcom_push=True,
+    dag=dag
+)
+
+
+[t1, t2] >> t3
