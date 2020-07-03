b4272a2f41bb02d3ab73c0de2926a85ae3fffb84
David Carlos <ddavidcarlos1392@gmail.com>
2020-07-03 16:40:27 -0300

    New repository structure

--- src/dashboard/comments.py
+++ src/dashboard/comments.py
@@ -2,13 +2,62 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import dash_html_components as html
 
 ej_comments_all = pd.read_json('/tmp/airflow/comments_only.json')
 
-ej_comments = pd.DataFrame(data=ej_comments_all, columns=['comentário_id', 'comentário', 'autor', 'concorda', 'discorda', 'pulados', 'participação'])
+ej_comments = pd.DataFrame(data=ej_comments_all, columns=['comentário_id', 'comentário', 'autor', 'concorda', 'discorda', 'pulados', 'participação', 'convergência'])
 ej_comments['concorda'] = ej_comments['concorda'].map(lambda x: x * 100)
 ej_comments['discorda'] = ej_comments['discorda'].map(lambda x: x * 100)
 ej_comments['pulados'] = ej_comments['pulados'].map(lambda x: x * 100)
-ej_comments['compilado'] = ''
+ej_comments['convergência'] = ej_comments['convergência'].map(lambda x: round(x * 100))
+ej_comments['geral'] = ''
 for index,value in enumerate(ej_comments['concorda']):
-    ej_comments['compilado'][index] = [ej_comments['concorda'][index], ej_comments['discorda'][index], ej_comments['pulados'][index]]
+    ej_comments['geral'][index] = [ej_comments['concorda'][index], ej_comments['discorda'][index], ej_comments['pulados'][index]]
+
+
+
+def generate_table_body(dataframe):
+    trs = []
+    for index in range(len(dataframe)):
+        tds = []
+        for col in dataframe.columns:
+            if(col == "convergência"):
+                tds.append(html.Td(str(dataframe.iloc[index][col]) + '%'))
+            if(col == "geral"):
+                bar = html.Div(
+                    style={},
+                    children=[
+                        html.Div(style={'borderStyle': 'solid', 'borderColor': 'grey', 'width': '100px', 'height': 20, 'display': 'flex'}, children=[
+                            html.Div(style={'backgroundColor': 'green', 'width': dataframe.iloc[index]['concorda'], 'height': 20}),
+                            html.Div(style={'backgroundColor': 'red', 'width': dataframe.iloc[index]['discorda'], 'height': 20}),
+                            html.Div(style={'backgroundColor': 'yellow', 'width': dataframe.iloc[index]['pulados'], 'height': 20})
+                        ]),
+                        html.Div(style={}, children=[
+                            html.Span(style={'color': 'green', 'fontSize': '11px', 'marginRight': '5px'}, children=str(round(dataframe.iloc[index]['concorda'])) + '%'),
+                            html.Span(style={'color': 'red', 'fontSize': '11px', 'marginRight': '5px'}, children=str(round(dataframe.iloc[index]['discorda'])) + '%'),
+                            html.Span(style={'color': 'yellow', 'fontSize': '11px', 'marginRight': '5px'}, children=str(round(dataframe.iloc[index]['pulados'])) + '%'),
+                        ]),
+                    ],
+                )
+                tds.append(html.Td(bar))
+            elif((col != "concorda") and (col != "discorda") and (col != "pulados") and (col != 'participação') and (col != 'convergência')):
+                tds.append(html.Td(dataframe.iloc[index][col]))
+        trs.append(html.Tr(tds))
+    return trs;
+
+def generate_table(dataframe):
+    ths = []
+    trs = []
+    for col in dataframe.columns:
+        if((col != 'concorda') and (col != 'discorda') and (col != 'pulados') and (col != 'participação')):
+            ths.append(html.Th(col))
+    trs = generate_table_body(dataframe)
+    return html.Table([
+            html.Thead(
+                html.Tr(ths)
+            ),
+            html.Tbody(
+                trs, id="table_body"
+            )
+        ])
--- src/dashboard/server.py
+++ src/dashboard/server.py
@@ -7,54 +7,11 @@ import pandas as pd
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
-from comments import ej_comments
+from comments import ej_comments, generate_table_body, generate_table
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-def generate_table_body(dataframe):
-    trs = []
-    for index in range(len(dataframe)):
-        tds = []
-        for col in dataframe.columns:
-            if(col == "compilado"):
-                bar = html.Div(
-                    style={},
-                    children=[
-                        html.Div(style={'borderStyle': 'solid', 'borderColor': 'black', 'width': '100px', 'height': 30, 'display': 'flex'}, children=[
-                            html.Div(style={'backgroundColor': 'green', 'width': dataframe.iloc[index]['concorda'], 'height': 30}),
-                            html.Div(style={'backgroundColor': 'red', 'width': dataframe.iloc[index]['discorda'], 'height': 30}),
-                            html.Div(style={'backgroundColor': 'yellow', 'width': dataframe.iloc[index]['pulados'], 'height': 30})
-                        ]),
-                        html.Div(style={}, children=[
-                            html.Span(style={'color': 'green', 'fontSize': '11px', 'marginRight': '5px'}, children=str(round(dataframe.iloc[index]['concorda'])) + '%'),
-                            html.Span(style={'color': 'red', 'fontSize': '11px', 'marginRight': '5px'}, children=str(round(dataframe.iloc[index]['discorda'])) + '%'),
-                            html.Span(style={'color': 'yellow', 'fontSize': '11px', 'marginRight': '5px'}, children=str(round(dataframe.iloc[index]['pulados'])) + '%'),
-                        ]),
-                    ],
-                )
-                tds.append(html.Td(bar))
-            elif((col != "concorda") and (col != "discorda") and (col != "pulados")):
-                tds.append(html.Td(dataframe.iloc[index][col]))
-        trs.append(html.Tr(tds))
-    return trs;
-
-def generate_table(dataframe):
-    ths = []
-    trs = []
-    for col in dataframe.columns:
-        if((col != 'concorda') and (col != 'discorda') and (col != 'pulados')):
-            ths.append(html.Th(col))
-    trs = generate_table_body(dataframe)
-    return html.Table([
-            html.Thead(
-                html.Tr(ths)
-            ),
-            html.Tbody(
-                trs, id="table_body"
-            )
-        ])
 
 
 order_options = ['comentário_id', 'concorda', 'discorda', 'pulados']
@@ -98,7 +55,7 @@ def order_table(_filter, participation):
         return generate_table_body(df)
 
 
-app.layout = html.Div(children=[
+app.layout = html.Div(style={"width": "80%", "margin": "auto"}, children=[
     html.H4(children='Votos e participação em todos os comentários, excluíndo os comentários que foram rejeitados para moderação.'),
     visualization
 ])
