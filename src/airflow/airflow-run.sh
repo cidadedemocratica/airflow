861bda55051282a65235f8575496f95de5a343a4
David Carlos <ddavidcarlos1392@gmail.com>
2020-07-20 20:56:20 -0300

    Run initdb on airflow-run script

--- src/airflow/airflow-run.sh
+++ src/airflow/airflow-run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-rm -rf /home/airflow/airflow.cfg
-ln -s /home/airflow/dags/src/airflow/airflow.cfg /home/airflow/
+rm -rf /var/airflow/airflow.cfg
+ln -s /home/airflow/dags/src/airflow/airflow.cfg /var/airflow/
 python /home/airflow/dags/src/dashboard/server.py &
+airflow initdb
 airflow webserver -p 8080 & 
 airflow scheduler
 
