6f88e8e494c8a0d5739afd43a48a3c86074e5d3c
David Carlos <ddavidcarlos1392@gmail.com>
2020-07-20 21:24:54 -0300

    Add more steps on airflow-run script

--- src/airflow/airflow-run.sh
+++ src/airflow/airflow-run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 rm -rf /var/airflow/airflow.cfg
 ln -s /home/airflow/dags/src/airflow/airflow.cfg /var/airflow/
+cp /home/airflow/dags/src/.analyticsreporting.dat /var/airflow
+cp /home/airflow/dags/src/.analyticsreporting.dat /tmp
+cp /home/airflow/dags/src/airflow/client_secrets.json /tmp/
+cp /home/airflow/dags/src/airflow/.*.env /tmp/
 python /home/airflow/dags/src/dashboard/server.py &
 airflow initdb
 airflow webserver -p 8080 & 
