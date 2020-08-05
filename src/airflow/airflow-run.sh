e392f132f6f07a5e68530f6ed81b4a2043b0bcc2
David Carlos <ddavidcarlos1392@gmail.com>
2020-08-05 12:43:49 -0300

    Removes airflow pid before starts server

--- src/airflow/airflow-run.sh
+++ src/airflow/airflow-run.sh
@@ -5,6 +5,7 @@ cp /home/airflow/dags/src/.analyticsreporting.dat /var/airflow
 cp /home/airflow/dags/src/.analyticsreporting.dat /tmp
 cp /home/airflow/dags/src/airflow/client_secrets.json /tmp/
 cp /home/airflow/dags/src/airflow/.*.env /tmp/
+rm /var/airflow/airflow-webserver.pid
 python /home/airflow/dags/src/dashboard/server.py &
 DB_FILE=/var/airflow/airflow.db
 if test ! -f "$DB_FILE"; then
@@ -12,4 +13,3 @@ if test ! -f "$DB_FILE"; then
 fi
 airflow webserver -p 8080 & 
 airflow scheduler
-
