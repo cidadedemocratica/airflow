521c12c6b7d6ac75aadadcdd689aec368fd5b39c
MarthaDs <marthadntas@gmail.com>
2020-08-12 15:40:17 -0300

    refator comments component, create comments service

--- src/dashboard/components/comments.py
+++ src/dashboard/components/comments.py
@@ -6,35 +6,25 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output
 
+from services.comments import CommentsService
+
 
 class CommentsComponent():
 
     def __init__(self, app):
         self.app = app
-        self.df = pd.DataFrame({})
-        self.clusters = pd.DataFrame({})
+        self.service = CommentsService()
+        self.df = self.service.df
+        self.clusters = self.service.clusters
         self.order_options = ['comentário_id',
                               'concorda', 'discorda', 'pulados', 'convergência']
         self.prepare()
-        self.callbacks()
 
     def prepare(self):
-        comments_df = pd.read_json('/tmp/comments.json')
-        self.df = pd.DataFrame(data=comments_df, columns=[
-            'comentário_id', 'comentário', 'autor', 'concorda', 'discorda', 'pulados', 'participação', 'convergência'])
-        self.df['concorda'] = self.df['concorda'].map(
-            lambda x: x * 100)
-        self.df['discorda'] = self.df['discorda'].map(
-            lambda x: x * 100)
-        self.df['pulados'] = self.df['pulados'].map(lambda x: x * 100)
-        self.df['participação'] = self.df['participação'].map(lambda x: x * 100)
-        self.df['convergência'] = self.df['convergência'].map(
-            lambda x: round(x * 100))
-        self.df['geral'] = ''
-        for index, value in enumerate(self.df['geral']):
-            self.df.loc[index,
-                        'geral'] = f"{self.df.loc[index, 'concorda']}, {self.df.loc[index, 'discorda']}, {self.df.loc[index, 'pulados']}"
-        self._merge_clusters_data()
+        try:
+            self.callbacks()
+        except:
+            pass
 
     def callbacks(self):
         if(not self.df.empty):
@@ -75,21 +65,6 @@ class CommentsComponent():
                 ])
             ])
         ])
-
-    def _merge_clusters_data(self):
-        self.clusters = pd.read_json('/tmp/clusters.json')
-        self.clusters = self.clusters.rename(columns={
-            "concorda": "cluster_concorda", "desagree": "cluster_discorda", "skip": "cluster_pulado", "comentário_id": "cluster_comentário_id"})
-        for index, value in enumerate(self.df['comentário']):
-            comment_clusters = self.clusters[self.clusters['comentário'] == value]
-            comment_clusters.loc[comment_clusters['comentário'] == value, 'cluster_concorda'] = comment_clusters['cluster_concorda'].map(
-                lambda x: x * 100)
-            comment_clusters.loc[comment_clusters['comentário'] == value, 'cluster_discorda'] = comment_clusters['cluster_discorda'].map(
-                lambda x: x * 100)
-            comment_clusters.loc[comment_clusters['comentário'] == value, 'cluster_pulado'] = comment_clusters['cluster_pulado'].map(
-                lambda x: x * 100)
-            for index2, cluster_name in enumerate(comment_clusters['cluster']):
-                self.df.loc[index, f'cluster_{cluster_name}'] = f"{comment_clusters.iloc[index2]['cluster_concorda']}, {comment_clusters.iloc[index2]['cluster_discorda']}, {comment_clusters.iloc[index2]['cluster_pulado']}"
 
     def _generate_table(self):
         ths = []
@@ -170,10 +145,7 @@ class CommentsComponent():
             return None
     
     def _generate_clusters_columns(self, col, df, index):
-        clusters_names = self.clusters.cluster.value_counts().keys()
-        clusters_names = clusters_names.map(
-            lambda cluster_name: f"cluster_{cluster_name}")
-        if(col in clusters_names):
+        if(col in CommentsService._get_clusters_name(self)):
             cluster_votes_statistics = df.iloc[index][col].split(',')
             bar = html.Div(
                 style={},
