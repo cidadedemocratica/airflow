e7e43902dd55aa73b06bcfc8ccaa14ee0b562176
David Carlos <ddavidcarlos1392@gmail.com>
2020-10-05 18:36:29 -0300

    Minor fix on airflow entrypoint script

--- src/airflow/airflow-run.sh
+++ src/airflow/airflow-run.sh
@@ -6,8 +6,8 @@ cp /home/airflow/dags/src/.analyticsreporting.dat /tmp
 cp /home/airflow/dags/src/airflow/client_secrets.json /tmp/
 cp /home/airflow/dags/src/airflow/.*.env /tmp/
 rm /var/airflow/airflow-webserver.pid
-python /home/airflow/dags/src/dashboard/server.py &
-airflow initdb
+#python /home/airflow/dags/src/dashboard/server.py &
+#airflow initdb
 python /home/airflow/dags/src/airflow/create_superuser.py
 airflow webserver -p 8080 & 
 airflow scheduler
