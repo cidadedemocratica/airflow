becaacf512b179fdb1443fae026be1c0d8756ac2
David Carlos <ddavidcarlos1392@gmail.com>
2020-10-07 19:25:50 -0300

    Fixes airflow entrypoint script

--- src/airflow/airflow-run.sh
+++ src/airflow/airflow-run.sh
@@ -6,11 +6,8 @@ cp /home/airflow/dags/src/.analyticsreporting.dat /tmp
 cp /home/airflow/dags/src/airflow/client_secrets.json /tmp/
 cp /home/airflow/dags/src/airflow/.*.env /tmp/
 rm /var/airflow/airflow-webserver.pid
-
-#TODO: only initdb if db does not exists
-#python /home/airflow/dags/src/airflow/create_superuser.py
-#airflow initdb
-
+airflow initdb 2> /dev/null
+python /home/airflow/dags/src/airflow/create_superuser.py 2> /dev/null
 python /home/airflow/dags/src/dashboard/server.py &
 airflow webserver -p 8080 & 
 airflow scheduler
