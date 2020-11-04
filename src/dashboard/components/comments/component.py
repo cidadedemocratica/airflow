22c8bde1ce47da7c477d5d0bb265c15397f786b9
David Carlos <ddavidcarlos1392@gmail.com>
2020-11-04 19:02:21 -0300

    Refactors comments table methods.

--- src/dashboard/components/comments/component.py
+++ src/dashboard/components/comments/component.py
@@ -10,67 +10,74 @@ import dash_bootstrap_components as dbc
 from components.comments.filters import FiltersComponent
 from components.utils.export import ExportsComponent
 
+CONVERSATION_URL = "https://www.ejplatform.org/conversations/56/ucc-conversa-1/"
+
 
 class CommentsComponent():
 
     def __init__(self, app):
         self.app = app
-        self.filters_component = FiltersComponent(app, self._generate_table)
-        self.comments = self.filters_component.comments
-        self.clusters = self.filters_component.clusters
-        self.conversation_url = "https://www.ejplatform.org/conversations/56/ucc-conversa-1/"
+        self.filters_component = FiltersComponent(app, self._get_table)
         self.export_component = ExportsComponent(
             "comments", app, self.filters_component)
 
     def render(self):
-            return html.Div(className="row", children=[
-                html.Div(className="col-12 mb-4", children=[
-                    html.Div(className="card shadow", children=[
-                        html.Div(className="card-header", children=[
-                            html.Div(children=[
-                                html.Span('Votos e participação em todos os comentários.', style={
-                                    "marginRight": 4}),
-                                html.I(className="fa fa-info-circle",
-                                       id='comments-component-title')
-                            ]),
-                            dbc.Tooltip(
-                                'Visualização que permite correlacionar como cada comentário performou no geral e nos clusters.',
-                                target='comments-component-title')
-
+        return html.Div(className="row", children=[
+            html.Div(className="col-12 mb-4", children=[
+                html.Div(className="card shadow", children=[
+                    html.Div(className="card-header", children=[
+                        html.Div(children=[
+                            html.Span('Votos e participação em todos os comentários.', style={
+                                "marginRight": 4}),
+                            html.I(className="fa fa-info-circle",
+                                   id='comments-component-title')
                         ]),
-                        html.Div(className="card-body", children=[
-                            html.Div(children=[
-                                dcc.Loading(id="comments_loader", type="default", color="#30bfd3", children=[
-                                    html.Div(id="commments_filters",
-                                             children=[self._get_table()])
-                                ]),
-                                html.Hr(),
-                            ])
+                        dbc.Tooltip(
+                            'Visualização que permite correlacionar como cada comentário performou no geral e nos clusters.',
+                            target='comments-component-title')
+                    ]),
+                    html.Div(className="card-body", children=[
+                        html.Div(children=[
+                            dcc.Loading(id="comments_loader", type="default", color="#30bfd3", children=[
+                                html.Div(children=[self._get_body()])
+                            ]),
+                            html.Hr(),
                         ])
                     ])
                 ])
             ])
+        ])
 
-    def _generate_table(self, comments):
+    def _get_body(self):
+        return html.Div(children=[
+            html.Div(className='comments-conversation-link',
+                     children=[
+                        html.Span(className="filter-title",
+                                  children=[html.Span("Conversa: "),
+                                            html.A(CONVERSATION_URL,
+                                                   href=CONVERSATION_URL,
+                                                   target="_blank")
+                                            ]),
+                        ]),
+            html.Div(children=[self.filters_component.render(),
+                               self.export_component.render()]),
+            html.Div(className='table-wrapper', children=[self._get_table()])
+        ])
+
+    def _get_table(self, comments=pd.DataFrame({}), clusters=pd.DataFrame({})):
         if (comments.empty):
-            return html.Div(className="row", children=[
-                html.Div(className="col-12 mb-4", children=[
-                    html.Div(className="card shadow", children=[
-                        html.Div(className="card-header", children=[
-                            'Votos e participação em todos os comentários.']),
-                        html.Div(className="card-body", children=[
-                            html.Div(children=[
-                                html.Span("Não há dados para apresentar"),
-                            ])
-                        ])
-                    ])
-                ])
-            ])
+            return self._empty_table()
+        return html.Table(className="comments-table", id="table_body", children=[
+            html.Thead(html.Tr(self._get_table_columns(comments))),
+            html.Tbody(self._get_table_body(comments, clusters)),
+        ])
+
+    def _get_table_columns(self, comments):
         ths = []
-        for col in self.comments.columns:
+        for col in comments.columns:
             if(col in ["comentário", "convergência"]):
                 ths.append(html.Th(col))
-        clusters = [
+        clusters_th = [
             html.Div("clusters", className='cluster-info'),
             html.Div(className="fa fa-info-circle", id='clusters-hover'),
             dbc.Tooltip(
@@ -83,76 +90,72 @@ class CommentsComponent():
                 placement="bottom",
                 className="hover-comments-bar"
             )]
-        ths.append(html.Th(clusters))
-        return html.Table(className="comments-table", id="table_body", children=[
-            html.Thead(
-                html.Tr(ths)
-            ),
-            html.Tbody(
-                self._generate_table_body(comments, self.clusters)
-            ),
-        ])
+        ths.append(html.Th(clusters_th))
+        return ths
 
-    def _generate_table_body(self, comments, clusters): 
+    def _empty_table(self):
+        return html.Div(className="card-body", children=[
+                        html.Div(id="table_body", children=[
+                                 html.Span("Não há dados para apresentar"), ])
+                        ])
+
+    def _get_table_body(self, comments, clusters):
         trs = []
-        for index in range(len(comments)):
+        for comment in comments.iterrows():
             tds = []
-            tds_clusters = []
-            for col in comments.columns:
-                if(col == "convergência"):
-                    tds.append(
-                        html.Td(str(round(comments.iloc[index][col] * 100)) + '%'))
-                geral_bar = self._get_geral_bar(
-                    col, comments, index)
-                cluster_bars = self._get_clusters_bars(
-                    col, comments, clusters, index)
-                if(geral_bar):
-                    tds_clusters.append(geral_bar)
-                if(cluster_bars):
-                    tds_clusters.append(cluster_bars)
-                elif(col == "comentário"):
-                    tds.append(
-                        html.Td(children=[
-                            html.Div(children=[comments.iloc[index][col]]),
-                            html.Div(className="comments-infos", children=[
-                                "id: " + str(comments.iloc[index]['comentário_id']),
-                                html.Br(),
-                                "autor: " + str(comments.iloc[index]['autor'])])
-                        ]))
-            tds.append(html.Td(className='clusters', children=tds_clusters))
+            row = comment[1]
+            tds.append(self._get_comentario_row(row))
+            tds.append(self._get_convergencia_row(row))
+            tds.append(self._get_clusters_row(row, clusters))
             trs.append(html.Tr(tds))
+
         return trs
 
-    def _get_geral_bar(self, col, comments, index):
-        if(col == "geral"):
-            dom_element = html.Div(
-                children=[
-                    html.Div(className='clusters-name', children=[
-                        html.Span(col)
-                    ]),
-                    html.Div(className='comment-bar', children=[
-                        html.Div(style={
-                            'backgroundColor': '#16ab39', 'width': comments.iloc[index]['concorda'] * 100, 'height': 20}),
-                        html.Div(style={
-                            'backgroundColor': '#de011e', 'width': comments.iloc[index]['discorda'] * 100, 'height': 20}),
-                        html.Div(style={
-                            'backgroundColor': '#042a46', 'width': comments.iloc[index]['pulados'] * 100, 'height': 20})
-                    ]),
-                    html.Div(style={}, children=[
-                        html.Span(className="agree", children=str(
-                            round(comments.iloc[index]['concorda'] * 100)) + '%'),
-                        html.Span(className="disagree", children=str(
-                            round(comments.iloc[index]['discorda'] * 100)) + '%'),
-                        html.Span(className="skipped", children=str(
-                            round(comments.iloc[index]['pulados'] * 100)) + '%'),
-                    ]),
-                    self._get_participation_bar(
-                        comments.iloc[index]['participação'] * 100)
-                ],
-            )
-            return dom_element
-        else:
-            return None
+    def _get_comentario_row(self, comment):
+        return html.Td(children=[
+            html.Div(children=[comment['comentário']]),
+            html.Div(className="comments-infos", children=[
+                "id: " +
+                str(comment['comentário_id']),
+                html.Br(),
+                "autor: " + str(comment['autor'])])
+        ])
+
+    def _get_convergencia_row(self, comment):
+        return html.Td(str(round(comment['convergência'] * 100)) + '%')
+
+    def _get_clusters_row(self, comment, clusters):
+        tds_clusters = []
+        tds_clusters.append(self._get_geral_bar(comment))
+        tds_clusters.extend(self._get_clusters_bars(comment, clusters))
+        return html.Td(className='clusters', children=tds_clusters)
+
+    def _get_geral_bar(self, comment):
+        return html.Div(
+            children=[
+                html.Div(className='clusters-name', children=[
+                    html.Span("geral")
+                ]),
+                html.Div(className='comment-bar', children=[
+                    html.Div(style={
+                        'backgroundColor': '#16ab39', 'width': comment.concorda * 100, 'height': 20}),
+                    html.Div(style={
+                        'backgroundColor': '#de011e', 'width': comment.discorda * 100, 'height': 20}),
+                    html.Div(style={
+                        'backgroundColor': '#042a46', 'width': comment.pulados * 100, 'height': 20})
+                ]),
+                html.Div(style={}, children=[
+                    html.Span(className="agree", children=str(
+                        round(comment.concorda * 100)) + '%'),
+                    html.Span(className="disagree", children=str(
+                        round(comment.discorda * 100)) + '%'),
+                    html.Span(className="skipped", children=str(
+                        round(comment.pulados * 100)) + '%'),
+                ]),
+                self._get_participation_bar(
+                    comment['participação'] * 100)
+            ],
+        )
 
     def _get_participation_bar(self, value):
         return html.Div(children=[
@@ -170,21 +173,17 @@ class CommentsComponent():
             ]),
         ])
 
-    def get_clusters_names(self, clusters):
+    def _get_clusters_bars(self, comment, clusters):
         clusters_names = clusters.cluster_name.value_counts().keys()
-        clusters_names = clusters_names.map(
-            lambda cluster_name: f"{cluster_name}")
-        return clusters_names
-
-    def _get_clusters_bars(self, col, comments, clusters, index):
-        if(col in self.get_clusters_names(clusters)):
-            cluster_votes_statistics = comments.iloc[index][col].split(',')
-            cluster_votes_participation = comments.iloc[index][f"{col}_participation"]
-            bar = html.Div(
+        tds = []
+        for cluster in clusters_names:
+            cluster_votes_statistics = comment[cluster].split(',')
+            cluster_votes_participation = comment[f"{cluster}_participation"]
+            tds.append(html.Div(
                 style={},
                 children=[
                     html.Div(className='clusters-name', children=[
-                        html.Span(col)
+                        html.Span(cluster)
                     ]),
                     html.Div(className='comment-bar', children=[
                         html.Div(style={
@@ -205,26 +204,5 @@ class CommentsComponent():
                     self._get_participation_bar(
                         float(cluster_votes_participation))
                 ],
-            )
-            return bar
-        return None
-
-    def _get_table(self):
-        return html.Div(children=[
-                html.Div(style={'display': 'flex', 'marginTop': '10px', 
-                                'alignItems': 'center', "fontWeight": "bold"},
-                    children=[
-                        html.Span(className="filter-title",
-                            children=[html.Span("Conversa: "),
-                                html.A(self.conversation_url,
-                                    href=self.conversation_url,
-                                        target="_blank")
-                            ]),
-                ]),
-                html.Div(children=[
-                    self.filters_component.render(),
-                    self.export_component.render()
-                 ]),
-                html.Div(className='table-wrapper',
-                    children=[self._generate_table(self.comments)])
-            ])
+            ))
+        return tds
--- src/dashboard/components/comments/filters.py
+++ src/dashboard/components/comments/filters.py
@@ -9,14 +9,15 @@ class FiltersComponent():
     """
         FiltersComponent adds inputs to filter CommentsComponent data.
     """
-    def __init__(self, app,render_comments):
+
+    def __init__(self, app, render_comments):
         self.service = CommentsService()
         self.comments = self.service.comments
         self.clusters = self.service.clusters
         self.app = app
         self.render_comments = render_comments
         self.order_options = ['concorda',
-                            'discorda', 'pulados', 'convergência']
+                              'discorda', 'pulados', 'convergência']
         self.set_filters_callbacks()
 
     def get_data_to_export(self):
@@ -31,33 +32,34 @@ class FiltersComponent():
         """
         return html.Div(children=[
             html.Div(style={'display': 'flex', 'marginTop': '10px', 'alignItems': 'center', 'width': '30%'}, children=[
-                    html.Span(style={"marginRight": 8, "fontWeight": "bold"},
-                              children="Participação acima de:"),
-                    dcc.Input(
+                html.Span(style={"marginRight": 8, "fontWeight": "bold"},
+                          children="Participação acima de:"),
+                dcc.Input(
                         id='participation',
                         type='number',
                         value='50',
                         debounce=True,
                         style={"flexGrow": 1, 'color': '#aaa', 'padding': '6px', 'opacity': '60%'}),
-                ]),
-                html.Div(style={'display': 'flex', 'marginTop': '10px', 'marginBottom': '18px', 'alignItems': 'center', 'width': '30%'}, children=[
-                    html.Span(style={"marginRight": 8, "fontWeight": "bold"},
-                              children="Ordenar por:"),
-                    dcc.Dropdown(
-                        id='_filter',
-                        options=[{'label': i, 'value': i}
-                                 for i in self.order_options],
-                        value='',
-                        style={"flexGrow": 1}
-                    ),
-                ]),
+            ]),
+            html.Div(style={'display': 'flex', 'marginTop': '10px', 'marginBottom': '18px', 'alignItems': 'center', 'width': '30%'}, children=[
+                html.Span(style={"marginRight": 8, "fontWeight": "bold"},
+                          children="Ordenar por:"),
+                dcc.Dropdown(
+                    id='_filter',
+                    options=[{'label': i, 'value': i}
+                             for i in self.order_options],
+                    value='',
+                    style={"flexGrow": 1}
+                ),
+            ]),
         ])
+
     def set_filters_callbacks(self):
         @self.app.callback(
             Output("table_body", 'children'),
             [Input('_filter', 'value'),
-            Input('app_reload', 'n_clicks'),
-            Input('participation', 'value')])
+             Input('app_reload', 'n_clicks'),
+             Input('participation', 'value')])
         def table_callback(_filter, app_reload, participation):
             self.reload_data_from_disk(app_reload)
             if(participation):
@@ -65,8 +67,9 @@ class FiltersComponent():
                 self.comments = self.comments[
                     self.comments['participação'] >= int(participation) / 100]
             if(_filter in self.order_options):
-                self.comments = self.comments.sort_values(by=_filter, ascending=False)
-            return self.render_comments(self.comments)
+                self.comments = self.comments.sort_values(
+                    by=_filter, ascending=False)
+            return self.render_comments(self.comments, self.clusters)
 
     def reload_data_from_disk(self, app_reload):
         if(app_reload != 0):
